4c61d7a4511c94ee4f22690f33c2f92279daba4d
Chaitanya Senapati <csenapati12@gmail.com>
2020-06-21 20:08:02 +0530

    Update run_tests_on_vm.sh

--- integration_tests/run_tests_on_vm.sh
+++ integration_tests/run_tests_on_vm.sh
@@ -3,3 +3,4 @@ wget https://releases.hashicorp.com/terraform/0.12.24/terraform_0.12.24_linux_am
 unzip terraform_0.12.24_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
 rm terraform_0.12.24_linux_amd64.zip
+#added comment
